b06a0dd2391069a52fa1ae9eb10a0938102a6f30
Ib Johansen <ib.johansen.jr@gmail.com>
2015-10-16 12:24:34 +0200

    edited server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxyServer();
 var app = express();
 
 var isProduction = process.env.NODE_ENV === 'production';
@@ -13,10 +11,6 @@ app.use(express.static(publicPath));
 
 app.get('*.png', function (req, res) {
     res.sendFile(publicPath + req.path);
-});
-
-proxy.on('error', function (e) {
-    console.log('Could not connect to proxy, please try again...');
 });
 
 app.listen(port, function () {
